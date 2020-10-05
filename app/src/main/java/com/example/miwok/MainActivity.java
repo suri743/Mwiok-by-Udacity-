998ef228db3728d9239393e1b07d8645b15fdf9b
Su Ri Sh <suri0393@gmail.com>
2020-10-05 16:01:08 +0530

    #Version_1.2

--- app/src/main/java/com/example/miwok/MainActivity.java
+++ app/src/main/java/com/example/miwok/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void openNumbers(View view){
-        Intent numbers = new Intent(getApplicationContext(), Numbers.class);
-        startActivity(numbers);
+        //Find the view that shows Numbers Category
+        TextView numbers = findViewById(R.id.numbers);
+
+        //Set a click listener on Numbers View
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, Numbers.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        //Find the view that shows Numbers Category
+        TextView familyMembers = findViewById(R.id.family);
+
+        //Set a click listener
+        familyMembers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent familyIntent = new Intent(MainActivity.this, FamilyMembers.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        //Find the view that shows colors Category
+        TextView colors = findViewById(R.id.colors);
+
+        //Set a click listener
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent familyIntent = new Intent(MainActivity.this, colors.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        //Find the view that shows colors Category
+        TextView phrases = findViewById(R.id.phrases);
+
+        //Set a click listener
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent familyIntent = new Intent(MainActivity.this, Phrases.class);
+                startActivity(familyIntent);
+            }
+        });
+
     }
 }
